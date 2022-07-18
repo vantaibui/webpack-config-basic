839d7555c05731947762798afcaa8b562d09deff
vantaibui <vantaibui92@gmail.com>
2022-07-18 07:10:39 +0700

    structure folder

---
+++ src/app/apis/axiosClient.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+const baseUrl = 'http://localhost:5000/api/v1';
+const getToken = () => localStorage.getItem('token');
+
+const axiosClient = axios.create({
+    baseURL: baseUrl,
+    paramsSerializer: params => queryString.stringify({params})
+});
+
+axiosClient.interceptors.request.use(async config => {
+    return {
+        ...config,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getToken()}`
+        }
+    }
+});
+
+axiosClient.interceptors.response.use(response => {
+    if(response && response.data) return response.data;
+    return response;
+}, err => {
+    if(!err.response) {
+        alert(err)
+    } throw err.response
+});
+
+export default axiosClient;
---
+++ src/app/utils/index.ts
@@ -0,0 +1,5 @@
+const verifyToken = (): string => {
+    return "abc"
+}
+
+export default verifyToken;
